35ee031e0d456410d63b990ed310506d16d7733c
fernflores0463 <fernando.flores0463@gmail.com>
2015-01-25 00:37:26 -0800

    f

--- templates/info.php
+++ templates/info.php
@@ -12,21 +12,60 @@
 			padding-top: 5px;
 		}
 
+		h3{
+			text-align: center;
+			font-style: oblique;
+			font-size: 130%;
+		}
+
+		p{
+			padding-left: 75px;
+			padding-right:75px;
+			font-size: 120%;
+		}
+
 		body{
 			background-image: url(static/assets/img/2.jpg);
 			background-size: 100%;
+
 			}
+
+		div.sub{
+			padding-top: 10px;
+			font-size: 120%;
+			padding-left: 75px;
+			padding-right:75px;
+
+		}
 
 		</style>
 </head>
 <body>
 	<h1>Name of Country's Culture</h1>
+	<div class="sub"></div>
 	<h3>Food</h3>
 	<p>Name of Country has grea food there is more than enough food to satidfy your needs and wants.</p>
-	<div></div>
+	</div>
+	<div class="sub">
 	<h3>Social Etiquette</h3>
+	Status is important and must be recognized by using the correct title when addressing someone. It is customary to use Sheikh (chief) (or Sheikha for a woman), Sayed (Mr.), Sayeda (Mrs.), etc. Arabs generally address people by their first names, so John Smith will be addressed as Mr. John.
+
+It is important to greet and acknowledge the most senior person in the room first.
+
+When doing business in the Middle East, handshakes are always used and can last a long time. Etiquette recommends that one waits for the other to withdraw their hand first before doing the same. For a man introduced to a woman, it is advisable to wait and see if a hand is extended. Particularly in public, Muslim women are unlikely to shake a man’s hand. A Western woman introduced to a Muslim man might also wait to see if he offers his hand.
+
+Always use the right hand. Among Muslims, the left hand is reserved for bodily hygiene and considered unclean. The right hand should be used for eating, shaking hands, or handing over an item.
+
+Do not be surprised if your hand is held while you are led somewhere. Holding hands among men is common and does not carry the same connotations as it does in the West.
+
+Many people in the Middle East claim a more modest area of personal space than is usual in the West. Accordingly, it can seem rude for an individual to step away when another individual is stepping closer.
+
+Special respect is paid to older people in many circumstances. This can include standing when older people enter a room, always greeting older people first, standing when speaking to one’s elders, and serving older people first at a meal.
+	</div>
+	<div class="sub">
 	<h3>Fashion in Country's Name</h3>
 	<p>The Islamic dress code is not compulsory, however there is a modest dress code in Dubai. Prohibitions on "indecent clothing" are an aspect of the UAE to which visitors are expected to conform. The UAE has enforced anti-indecency prohibitions in all public places (aside from beaches, clubs and bars). Sleeveless tops and short dresses are prohibited at Dubai's malls. Some expats and tourists disregard the official dress code and laws. The dress code is part of Dubai's criminal law. Expats are prohibited from dressing inappropriately by revealing too much of their skin. Clothes must be in appropriate lengths.<p>
+	</div>
 
 		
 
