58245d98f35a929b4c9bcb316ac3c8b344e9c8ef
angela278 <angela_zhang278@hotmail.com>
2015-01-24 19:31:33 -0800

    Revert bd57394..9612441
    This rolls back to commit bd57394373a7ba1c498f551c86962f66b7c2338c.

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/info")
-def info():
-  return render_template("info.html")
  
 # @app.route('/builder')
 # def builder():
