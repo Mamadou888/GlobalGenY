8de16afbb7dc1377433b267c346307e9837fd71d
fernflores0463 <fernando.flores0463@gmail.com>
2015-01-24 23:43:59 -0800

    g

--- templates/info.php
+++ templates/info.php
@@ -3,17 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php echo "geoip_country_code_by_name(hostname)";?></title>
-</head>
-
-<body>
-	<h1>Name of Country's Culture</h1>
-	<h3>Food</h3>
-	<p>Name of Country has grea food there is more than enough food to satidfy your needs and wants.</p>
-	<div></div>
-	<h3>Social Etiquette</h3>
-	<h3>Fashion in Country's Name</h3>
-	<p>Fashion in <p>
-		<style type="text/css">
+<style type="text/css">
 		h1{
 			text-align: center;
 			text-transform: capitalize;
@@ -25,6 +15,16 @@
 			}
 
 		</style>
+</head>
+<body>
+	<h1>Name of Country's Culture</h1>
+	<h3>Food</h3>
+	<p>Name of Country has grea food there is more than enough food to satidfy your needs and wants.</p>
+	<div></div>
+	<h3>Social Etiquette</h3>
+	<h3>Fashion in Country's Name</h3>
+	<p>Fashion in <p>
+		
 
 
 
